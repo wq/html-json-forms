1f2b497f65dce80035eaf23acdf0dce292f2952e
S. Andrew Sheppard <andrew@wq.io>
2016-03-07 15:24:27 -0600

    v0.1.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def parse_markdown_readme():
 
 setup(
     name='html-json-forms',
-    version='0.1.0',
+    version='0.1.1',
     author='S. Andrew Sheppard',
     author_email='andrew@wq.io',
     url='https://github.com/wq/html-json-forms',
